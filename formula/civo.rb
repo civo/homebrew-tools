7070693d21bdad423231cd48f0adc46fe9ef976c
Andy Jeffries <andy@civo.com>
2020-05-13 17:06:28 +0100

    Update release 0.1.6

--- formula/civo.rb
+++ formula/civo.rb
@@ -3,9 +3,9 @@ require "formula"
 class Civo < Formula
   desc "CLI for managing Civo resources"
   homepage "https://github.com/civo/cli-go"
-  url "https://github.com/civo/cli-go/releases/download/v0.1.5/civo_0.1.5_darwin_amd64.tar.gz"
+  url "https://github.com/civo/cli-go/releases/download/v0.1.6/civo_0.1.6_darwin_amd64.tar.gz"
   # sha256 of the URL above's contents, not the extracted binary
-  sha256 "867f060b1b62237b5a9be3a695dbb694a76fd06c1c71e37e9582594308f033fc"
+  sha256 "01447de5f8dfbd6558c5284529f5dfc9ca977451d41ae47209bd3294effa855c"
   head "https://github.com/civo/cli-go.git"
 
   def install
@@ -14,6 +14,6 @@ class Civo < Formula
 
   # Homebrew requires tests.
   test do
-    assert_match "0.1.5", shell_output("#{bin}/civo version -q", 2)
+    assert_match "0.1.6", shell_output("#{bin}/civo version -q", 2)
   end
 end
