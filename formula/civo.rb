1f6110ba2943d46d5fdd758dbcbb1d8de9ffe498
Andy Jeffries <andy@andyjeffries.co.uk>
2020-05-13 16:20:13 +0100

    Update SHA hash

--- formula/civo.rb
+++ formula/civo.rb
@@ -4,7 +4,8 @@ class Civo < Formula
   desc "CLI for managing Civo resources"
   homepage "https://github.com/civo/cli-go"
   url "https://github.com/civo/cli-go/releases/download/v0.1.5/civo_0.1.5_darwin_amd64.tar.gz"
-  sha256 "c7dcd4a8ce25adeaf707acabc099789ac68cdaff29f8f1115118c30070280cbd"
+  # sha256 of the URL above's contents, not the extracted binary
+  sha256 "867f060b1b62237b5a9be3a695dbb694a76fd06c1c71e37e9582594308f033fc"
   head "https://github.com/civo/cli-go.git"
 
   def install
