766372fd8de867f900bc9c555b6346cb113fbb19
civobot <hello@civo.com>
2020-11-18 01:58:08 +0000

    Brew formula update for civo version v0.6.42

--- Formula/civo.rb
+++ Formula/civo.rb
@@ -2,26 +2,24 @@
 class Civo < Formula
   desc "CLI for managing Civo resources."
   homepage "https://github.com/civo/cli"
-  version "0.6.41"
+  version "0.6.42"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/civo/cli/releases/download/v0.6.41/civo-0.6.41-darwin-amd64.tar.gz"
-    sha256 "697f3a98ae615d58069565f9ec49ec0c41284996f948cd54a6b4eeb7d683293e"
-  elsif OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/civo/cli/releases/download/v0.6.41/civo-0.6.41-linux-amd64.tar.gz"
-      sha256 "9319bad920dea1bb103d691dfb062c96d0283c7ca77b2a3d5078253e1d88e374"
-    end
-    if Hardware::CPU.arm?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/civo/cli/releases/download/v0.6.41/civo-0.6.41-linux-arm64.tar.gz"
-        sha256 "87d79cf2c4e8661fbebca528ff706dd64bb6920989ab0afe4b7bcb9b1e79da93"
-      else
-        url "https://github.com/civo/cli/releases/download/v0.6.41/civo-0.6.41-linux-arm.tar.gz"
-        sha256 "a1173c08fc2833591dee129cfe1f6c7ca21600b508f75a6014744319934ce211"
-      end
-    end
+    url "https://github.com/civo/cli/releases/download/v0.6.42/civo-0.6.42-darwin-amd64.tar.gz"
+    sha256 "2f6e10205b1d8084cbf37290501a0f9c49a2feccec4ba8ec7f466906f56f9e82"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/civo/cli/releases/download/v0.6.42/civo-0.6.42-linux-amd64.tar.gz"
+    sha256 "7ca810edf41a9400441587f09795b2d3c10b57683068d6f8de599d817640accf"
+  end
+  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+    url "https://github.com/civo/cli/releases/download/v0.6.42/civo-0.6.42-linux-arm.tar.gz"
+    sha256 "2f4473964491811d0055ab26e733c0c8e7b239d564952042a8346537a40edd33"
+  end
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/civo/cli/releases/download/v0.6.42/civo-0.6.42-linux-arm64.tar.gz"
+    sha256 "2a572b5eaf4a5730a465b8d31b1543526cc38f05948d730dc9bf756919fe291c"
   end
 
   def install
