491a23ceaa54a8f7bc6ae381a60dfb3c2e4ec37a
civobot <hello@civo.com>
2020-12-10 17:12:59 +0000

    Brew formula update for civo version v0.6.46

--- Formula/civo.rb
+++ Formula/civo.rb
@@ -2,24 +2,24 @@
 class Civo < Formula
   desc "CLI for managing Civo resources."
   homepage "https://github.com/civo/cli"
-  version "0.6.45"
+  version "0.6.46"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/civo/cli/releases/download/v0.6.45/civo-0.6.45-darwin-amd64.tar.gz"
-    sha256 "30c2d53d2407e94d2c7d98baa2648ea0bc066db6671ebcf115a67681953ed639"
+    url "https://github.com/civo/cli/releases/download/v0.6.46/civo-0.6.46-darwin-amd64.tar.gz"
+    sha256 "22f7d8816802a66ec34b6987c4c2aded8bb56e8c444128798cb37e20e9d72a8b"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/civo/cli/releases/download/v0.6.45/civo-0.6.45-linux-amd64.tar.gz"
-    sha256 "07dbae949424ad7074f2de26d648f06787e37ccfcbadae58013ce198097d24c1"
+    url "https://github.com/civo/cli/releases/download/v0.6.46/civo-0.6.46-linux-amd64.tar.gz"
+    sha256 "c8aaf0c9f54c824b5937e48fb1b4c677c2a3563b914777d45db8d7d59c506233"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/civo/cli/releases/download/v0.6.45/civo-0.6.45-linux-arm.tar.gz"
-    sha256 "45adefffc93e616e082f60e245146f698eb3d3c5d12eb9fc4acfa8c7acd5ba03"
+    url "https://github.com/civo/cli/releases/download/v0.6.46/civo-0.6.46-linux-arm.tar.gz"
+    sha256 "9a90b427c82328336c321751f6dc4c33610ab1b77173deaf265d2f3e53b29197"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/civo/cli/releases/download/v0.6.45/civo-0.6.45-linux-arm64.tar.gz"
-    sha256 "3c4a6b3d327bc03fa777926ceb7d667c72a462d3ac91f6de9af4a0fc4269df7e"
+    url "https://github.com/civo/cli/releases/download/v0.6.46/civo-0.6.46-linux-arm64.tar.gz"
+    sha256 "63f62b1d90e18e90bd8f3e917da31e96eb6dcdbe998d3b2b5455e07e958938cb"
   end
 
   def install
