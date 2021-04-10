7bf453157c69123090c91fb5b3c809ee613e1ed1
civobot <hello@civo.com>
2021-04-10 05:13:16 +0100

    Brew formula update for kubemart version v0.0.27

--- Formula/kubemart.rb
+++ Formula/kubemart.rb
@@ -5,28 +5,28 @@
 class Kubemart < Formula
   desc "CLI for interacting with your Kubemart apps"
   homepage "https://github.com/kubemart/kubemart-cli"
-  version "0.0.26"
+  version "0.0.27"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/kubemart/kubemart-cli/releases/download/v0.0.26/kubemart-0.0.26-darwin-amd64.tar.gz"
-    sha256 "c301e73289d021826bf13bfb56c4b4605f1765bd6c858339fe137bbe32a5c5c0"
+    url "https://github.com/kubemart/kubemart-cli/releases/download/v0.0.27/kubemart-0.0.27-darwin-amd64.tar.gz"
+    sha256 "7b19a8e5909c8e7cb60298fa2183b61b1c60f4fccbbc09059a91f0138ed627b8"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/kubemart/kubemart-cli/releases/download/v0.0.26/kubemart-0.0.26-darwin-arm64.tar.gz"
-    sha256 "03e039db92f934e28fdddad85669db652bf5ecdb12c29f4a8f52b4ea4ae95a03"
+    url "https://github.com/kubemart/kubemart-cli/releases/download/v0.0.27/kubemart-0.0.27-darwin-arm64.tar.gz"
+    sha256 "96e00d3246f55720b93e652b6508cd2d107336eaaf99efff66340364ba140b5d"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/kubemart/kubemart-cli/releases/download/v0.0.26/kubemart-0.0.26-linux-amd64.tar.gz"
-    sha256 "b0a0047c4103bbf2afda9d0ec9d6fb630267349619f8ee3235e5f1352e54118f"
+    url "https://github.com/kubemart/kubemart-cli/releases/download/v0.0.27/kubemart-0.0.27-linux-amd64.tar.gz"
+    sha256 "a71daae6370db6671aeb71d979f3777623b9d6d263fbe7e8dc87d2f8d9e2777e"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/kubemart/kubemart-cli/releases/download/v0.0.26/kubemart-0.0.26-linux-arm.tar.gz"
-    sha256 "2a4c21992737bb42c5735e1a82b5d7dc43d44546d51f4e744706b819c82c2449"
+    url "https://github.com/kubemart/kubemart-cli/releases/download/v0.0.27/kubemart-0.0.27-linux-arm.tar.gz"
+    sha256 "8cf5a4582d5e3b95657f1d720dc64c1e7e34ee4918fa956e4d4fa1d9ab455f3d"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/kubemart/kubemart-cli/releases/download/v0.0.26/kubemart-0.0.26-linux-arm64.tar.gz"
-    sha256 "f4c46aa40aff47f2261883ab8969240a3f9e7e0d703c72f3c911e0857ccedc53"
+    url "https://github.com/kubemart/kubemart-cli/releases/download/v0.0.27/kubemart-0.0.27-linux-arm64.tar.gz"
+    sha256 "3bb4ae2483b72f0b005ec4f493485962a733c9bc3b44012b21ea23a6f65828d3"
   end
 
   def install
